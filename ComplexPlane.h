fbd2ad2fbd6652dbbe683553d9d1fb1db696d6c0
julianrambob <julianrambob@gmail.com>
2023-04-25 15:08:09 -0700

    header file

---
+++ ComplexPlane.h
@@ -0,0 +1,28 @@
+#pragma once
+
+class ComplexPlane
+{
+	private:
+		Vector2f m_mouseLocation;
+		View m_view;
+		int m_zoomCount;
+		float m_aspectRatio;
+
+	public:
+
+		const unsigned int MAX_ITER = 64;
+		const float BASE_WIDTH = 4.0;
+		const float BASE_HEIGHT = 4.0;
+		const float BASE_ZOOM = 0.5;
+
+		ComplexPlane(float aspectRatio);
+		void zoomIn();
+		void zoomOut();
+		void setCenter(Vector2f coord);
+		View getView();
+		void setMouseLocation(Vector2f coord);
+		void loadText(Text& text);
+		static size_t counterIterations(Vector2f coord);
+		static void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
+
+};
